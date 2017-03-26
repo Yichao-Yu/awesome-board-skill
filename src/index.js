fc777c0e770986d79fe24638f2da82966a2541e9
yichaoyu <yichaoyu@gmail.com>
2017-03-26 18:42:11 -0400

    add .gitignore add firebase api enable firebase hide config detail

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
 'use strict';
 const Alexa = require("alexa-sdk");
-const appId = ''; //'amzn1.echo-sdk-ams.app.your-skill-id';
+const firebase = require("firebase");
+const config = require("config.json");
+const appId = config.appId;
+
+// Initialize Firebase
+firebase.initializeApp(config.firebaseConfig);
 
 exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
     alexa.appId = appId;
-    // alexa.dynamoDBTableName = 'awesomeBoard'; TODO add dynamoDB integration
     alexa.registerHandlers(handlers, awesomeHandlers);
     alexa.execute();
 };
-
-const testUsers = [
-    "yyc",
-    "johndoe"
-];
-
-const sampleAwesomeness = [
-    {
-        "user": "yyc",
-        "awesomeness": "created this awesome board skill"
-    },
-    {
-        "user": "johndoe",
-        "awesomeness": "enabled this awesome board skill"
-    }
-];
-
-const awesomeBoard = {
-    "users": testUsers,
-    "awesomeList": sampleAwesomeness
-}
 
 const handlers = {
     'NewSession': function () {
@@ -37,47 +20,76 @@ const handlers = {
             'or say add user to a new Awesome Board user. ' +
             'Say cancel to quit.');
     },
-    "AMAZON.StopIntent": function () {
+    'AMAZON.StopIntent': function () {
         this.emit(':tell', "Goodbye!");
     },
-    "AMAZON.CancelIntent": function () {
+    'AMAZON.CancelIntent': function () {
         this.emit(':tell', "Goodbye!");
     },
     'SessionEndedRequest': function () {
-        this.emit(":tell", "Goodbye!");
+        this.emit(':tell', "Goodbye!");
+    },
+    'Unhandled': function () {
+        this.emit(':tell', "Sorry, I didn\'t get that.");
     }
 };
 
 const awesomeHandlers = {
     'AwesomeBoardSummaryIntent': function () {
-        let numOfUsers = awesomeBoard.users.length;
-        let numOfAwesomeness = awesomeBoard.awesomeList.length;
-        if (numOfAwesomeness > 0) {
-            this.emit(':ask', 'Yes we are awesome!' +
-                `There are ${numOfAwesomeness} awesomeness from ${numOfUsers} users` +
-                'To check the awesomeness of an user, ask why user, johndoe, for example, is awesome.');
-        } else {
-            this.emit(':ask', 'No, we are awful. ' +
-                'Say add user and spell user name to add user to the Awesome Board. ' +
-                'Say add new awesomeness and user name to make us awesome! ');
-        }
-    },
-    'AwesomenessByUserIntent': function () {
-        let awesomeUser = this.event.request.intent.slots.user.value;
-        let awesomeness = [];
-        sampleAwesomeness.forEach(function (e) {
-            if (e.user === awesomeUser) {
-                awesomeness.push(e.awesomeness)
+        console.log("AwesomeBoardSummaryIntent event", JSON.stringify(this.event));
+        let numOfUsers = 0;
+        let numOfAwesomeness = 0;
+        let awesomenessesRef = firebase.database().ref("awesomenesses");
+        let self = this;
+        awesomenessesRef.once('value').then(function (snapshot) {
+            numOfUsers = snapshot.numChildren();
+            console.log("Current awesome board users ", numOfUsers);
+            snapshot.forEach(function (childSnapshot) {
+                numOfAwesomeness += childSnapshot.numChildren();
+            });
+            console.log("Current awesome board awesomeness ", numOfAwesomeness);
+            if (numOfAwesomeness > 0) {
+                self.emit(':ask', 'Yes we are awesome! ' +
+                    `There are ${numOfAwesomeness} awesomeness from ${numOfUsers} users. ` +
+                    'To check the awesomeness of an user, ask why user, johndoe, for example, is awesome.');
+            } else {
+                self.emit(':ask', 'No, we are awful. ' +
+                    'Say add user and spell user name to add user to the Awesome Board. ' +
+                    'Say add new awesomeness and user name to make us awesome! ');
             }
         });
-        if (awesomeness.length > 0) {
-            let output = "";
-            awesomeness.forEach(function (e) {
-                output += e + ".";
-            });
-            this.emit(':tell', `${awesomeUser} is awesome. Because ${awesomeUser} ${output}`);
-        } else {
-            this.emit(':tell', `Sorry. User ${awesomeUser} is not awesome. Bye.`);
-        }
-    }
+    },
+    'AwesomenessByUserIntent': function () {
+        console.log("AwesomenessByUserIntent event", JSON.stringify(this.event));
+        let awesomeUser = this.event.request.intent.slots.user.value;
+        let awesomenessList = [];
+        let awesomenessesRef = firebase.database().ref("awesomenesses/" + awesomeUser);
+        let self = this;
+        awesomenessesRef.once('value').then(function (snapshot) {
+            if (!snapshot.hasChildren()) {
+                console.log(`User ${awesomeUser} does not exist.`);
+                self.emit(':tell', `Sorry. User <say-as interpret-as="spell-out">${awesomeUser}</say-as> does not exist. Bye.`);
+            } else {
+                snapshot.forEach(function (childSnapshot) {
+                    console.log(JSON.stringify(childSnapshot));
+                    awesomenessList.push(childSnapshot.val())
+                });
+                if (awesomenessList.length > 0) {
+                    console.log(`User has ${awesomenessList.length} awesomeness.`);
+                    let output = "";
+                    let cnt = 1;
+                    awesomenessList.forEach(function (e) {
+                        output += cnt + ": " + e + " ";
+                        cnt++;
+                    });
+                    self.emit(':tell', `<say-as interpret-as="spell-out">${awesomeUser}</say-as> is awesome. ` +
+                        `Because <say-as interpret-as="spell-out">${awesomeUser}</say-as> ${output}`);
+                } else {
+                    console.log(`User ${awesomeUser} does not have awesomeness.`);
+                    self.emit(':tell', `Sorry. User <say-as interpret-as="spell-out">${awesomeUser}</say-as> is not awesome. Bye.`);
+                }
+            }
+        });
+    },
+
 }
