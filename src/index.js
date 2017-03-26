c1d578aaa90b433f9dbe75599919a1c24b9e54d7
yichaoyu <yichaoyu@gmail.com>
2017-03-26 15:25:10 -0400

    add sample utternaces and intent schema

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 'use strict';
-var Alexa = require("alexa-sdk");
-var appId = ''; //'amzn1.echo-sdk-ams.app.your-skill-id';
+const Alexa = require("alexa-sdk");
+const appId = ''; //'amzn1.echo-sdk-ams.app.your-skill-id';
 
 exports.handler = function (event, context, callback) {
-    var alexa = Alexa.handler(event, context);
+    const alexa = Alexa.handler(event, context);
     alexa.appId = appId;
     // alexa.dynamoDBTableName = 'awesomeBoard'; TODO add dynamoDB integration
     alexa.registerHandlers(newSessionHandlers);
